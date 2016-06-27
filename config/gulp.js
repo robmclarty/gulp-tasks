5980275b1be00b527648579ff7e677f5f417dbc1
Robert McLarty <rob.m@myplanet.com>
2016-06-27 11:32:11 -0400

    update some config settings with more comments

--- config/gulp.js
+++ config/gulp.js
@@ -1,23 +1,30 @@
 'use strict';
 
-const serverConf = require('./server');
-
-module.exports = Object.freeze({
+module.exports = {
+  // These paths make certain assumptions about where your files are stored on
+  // your remote server. Change as necessary. They are currently setup for
+  // standard filesystem hierarchy, but feel free to use your own system.
   deploy: {
-    host: process.env.SERVER_HOST || 'telus-jwt',
-    appRoot: `/opt/${ serverConf.appName }`,
-    staticRoot: `/srv/opt/${ serverConf.appName }`,
-    pm2Conf: `/etc/opt/${ serverConf.appName }/pm2.json`
+    host: process.env.SERVER_HOST || '<your-host-name>',
+    appRoot: `/opt/<your-app-name>`,
+    staticRoot: `/srv/opt/<your-app-name>`,
+    pm2Conf: `/etc/opt/<your-app-name>/pm2.json`
   },
+
+  // These don't need to be changed. They are just temporarily hold references
+  // to the file names that need to change while the build process runs.
   rev: {
     assets: 'rev-assets-manifest.json',
     javascripts: 'rev-js-manifest.json',
     stylesheets: 'rev-css-manifest.json'
   },
+
+  // You might want to build into a different directory (e.g., maybe one called
+  // "build" or "dist" rather than "public").
   build: {
     root: './public',
     stylesheets: './public/stylesheets',
     javascripts: './public/javascripts',
     admin: './public/admin'
   }
-});
+};
